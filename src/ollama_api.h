aadc3a2b0ccc225a9236e0abe534a656bd0ba2f7
Jaime Lopez <jaime.lopez@datainquiry.dev>
2025-08-26 20:52:41 -0400

    Code cleaning

--- src/ollama_api.h
+++ src/ollama_api.h
@@ -3,7 +3,6 @@
 
 #include <stddef.h>
 
-// Forward declaration
 typedef struct AppData AppData;
 
 typedef struct {
--- src/ollama_chat.c
+++ src/ollama_chat.c
@@ -1,38 +1,28 @@
-// ollama_chat.c - Ollama GTK Chat Application in C
+#include <stdlib.h>
 #include "app_data.h"
 #include "ui.h"
 #include "ollama_api.h"
 #include "history.h"
 #include "config.h"
 
-#include <stdlib.h>
-
 static AppData *app_data = NULL;
 
 static void on_activate(GtkApplication *app, gpointer user_data) {
-    (void)user_data;
+    (void) user_data;
     app_data = g_malloc0(sizeof(AppData));
     app_data->app = app;
     app_data->response_buffer = g_string_new("");
-
-    // Set the application icon
     GtkIconTheme *icon_theme = gtk_icon_theme_get_for_display(gdk_display_get_default());
     gtk_icon_theme_add_search_path(icon_theme, "/usr/share/icons/hicolor/scalable/apps");
-    
     config_init(app_data);
-
     const char *ollama_host = g_getenv("OLLAMA_HOST");
     if (ollama_host) {
         if (app_data->base_url) g_free(app_data->base_url);
         app_data->base_url = g_strdup(ollama_host);
     }
-    
     history_init(app_data);
-    
     ui_build(app, app_data);
-    
     history_load_chats(app_data);
-
     if (g_list_model_get_n_items(G_LIST_MODEL(app_data->history_store)) == 0) {
         history_start_new_chat(app_data);
     } else {
@@ -43,19 +33,15 @@ static void on_activate(GtkApplication *app, gpointer user_data) {
             gtk_list_box_select_row(app_data->history_list_box, first_row);
         }
     }
-    
     api_check_connection(app_data);
 }
 
 int main(int argc, char *argv[]) {
     api_init();
-    
     GtkApplication *app = gtk_application_new(
         "dev.datainquiry.ollama-chat", G_APPLICATION_DEFAULT_FLAGS);
     g_signal_connect(app, "activate", G_CALLBACK(on_activate), NULL);
-    
     int status = g_application_run(G_APPLICATION(app), argc, argv);
-    
     if (app_data) {
         config_save(app_data);
         history_save_chat(app_data);
@@ -86,9 +72,7 @@ int main(int argc, char *argv[]) {
         g_string_free(app_data->response_buffer, TRUE);
         g_free(app_data);
     }
-    
     g_object_unref(app);
     api_cleanup();
-    
     return status;
 }
